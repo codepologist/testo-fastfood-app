cc8c7d2665fc686151dd1b411cf7ab88dd8693b6
codepologist <ombu.oneal@yahoo.com>
2025-04-11 12:29:09 +0100

    feat(layout): Add metadata for SEO and app branding
    - Included keywords for better search engine optimization.
    - Added author information and application name.
    - Defined viewport settings for responsive design.
    - Configured icons for favicon and Apple touch devices.

--- app/layout.js
+++ app/layout.js
@@ -4,45 +4,35 @@ export const metadata = {
   title: "Testo Pizza and Fast Food Ordering App",
   description:
     "Testo - Your ultimate web app for ordering delicious pizza and fast food with ease and convenience.",
+  keywords: [
+    "Testo",
+    "pizza",
+    "burgers",
+    "sushi",
+    "steak",
+    "fast food",
+    "restaurant menu",
+    "food delivery",
+  ],
+  authors: [{ name: "O'Neal Ombu" }],
+  applicationName: "Testo App",
+  generator: "Next.js",
+  viewport: "width=device-width, initial-scale=1.0",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: [
+      { url: "/apple-touch-icon.png" },
+      { url: "/apple-touch-icon-152x152.png", sizes: "152x152" },
+      { url: "/apple-touch-icon-120x120.png", sizes: "120x120" },
+      { url: "/apple-touch-icon-76x76.png", sizes: "76x76" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="author" content="O'Neal Ombu" />
-        <meta
-          name="description"
-          content="Testo - Your ultimate web app for ordering delicious pizza and fast food with ease and convenience."
-        />
-        <meta
-          name="keywords"
-          content="Testo - Your go-to app for quick and delicious food, from pizza and burgers to sushi, steak, and more, straight from your favorite restaurant menu."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-touch-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-touch-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/apple-touch-icon-76x76.png"
-        />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" href="/apple-touch-icon.png" type="image/x-icon" />
-      </head>
       <body>{children}</body>
     </html>
   );
